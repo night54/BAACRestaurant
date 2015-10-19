3708d1dface876d578335ee75bd1895d999d9c35
night <krongrat.baac@gmail.com>
2015-10-19 12:59:55 +0700

    start github

---
+++ app/src/main/java/th/or/baac/oa/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,18 @@
+package th.or.baac.oa.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 10/19/2015.
+ */
+public class MyOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyOpenHelper(Context context) {
+
+    } //End of Constructor
+
+} // End of Main Class
